69e22ac6d8ff8902280dc76d133c50cd80b9c249
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2023-05-29 01:59:04 +0400

    Remove lego base folder too

--- lego-test/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ACME_DNS_STORAGE_PATH=./credentials.json
-export ACME_DNS_API_BASE=https://api.getlocalcert.net/api/v1/acme-dns-compat
-./lego --accept-tos --email moonrockseven@gmail.com --dns acme-dns --domains 1.localhostcert.net --server https://acme-staging-v02.api.letsencrypt.org/directory run
-
-# error: it looks like this tries to setup a new account?
-# needs debugging
-
