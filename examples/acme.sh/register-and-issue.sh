c6456a601ca5f8edddf857b21fc034d90c4cd9cc
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2023-06-27 10:28:40 -0400

    Same domain?

--- examples/acme.sh/register-and-issue.sh
+++ examples/acme.sh/register-and-issue.sh
@@ -2,36 +2,24 @@
 
 set -e
 
-acme_sh_extra_flags=""
-
-while [ ${#} -gt 0 ]; do
-  case "${1}" in
-  --staging)
-    acme_sh_extra_flags+=" --staging "
-    ;;
-  *)
-    echo "Unknown parameter : $1"
-    exit 1
-    ;;
-  esac
-  shift 1
-done
-
-
 export LOCALCERT_API=https://api.getlocalcert.net/api/v1
 export ACMEDNS_BASE_URL=${LOCALCERT_API}/acme-dns-compat
 
 # Register a fresh instant domain
 curl -X POST ${LOCALCERT_API}/register > /tmp/creds.json
 
-# Issue a certificate with LE (staging env)
+# Issue a certificate
 export ACMEDNS_SUBDOMAIN=$(jq -r .subdomain /tmp/creds.json)
 export ACMEDNS_FULLDOMAIN=$(jq -r .fulldomain /tmp/creds.json)
 export ACMEDNS_USERNAME=$(jq -r .username /tmp/creds.json)
 export ACMEDNS_PASSWORD=$(jq -r .password /tmp/creds.json)
 
-~/.acme.sh/acme.sh --issue --dnssleep 1 --dns dns_acmedns --force -d ${ACMEDNS_FULLDOMAIN} ${acme_sh_extra_flags}
+# First staging environment
+~/.acme.sh/acme.sh --issue --dnssleep 1 --dns dns_acmedns --force -d ${ACMEDNS_FULLDOMAIN} --staging
 
-# Cleanup for repeated runs
-~/.acme.sh/acme.sh --remove -d ${ACMEDNS_FULLDOMAIN} ${acme_sh_extra_flags}
+# Cleanup
+~/.acme.sh/acme.sh --remove -d ${ACMEDNS_FULLDOMAIN} --staging
+
+# Try prod if staging worked
+~/.acme.sh/acme.sh --issue --dnssleep 1 --dns dns_acmedns --force -d ${ACMEDNS_FULLDOMAIN}
 
