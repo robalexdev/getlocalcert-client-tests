02f07bf812a66a33ff41c4cc652bc1368785b9b5
Robert Alexander <36200436+robalexdev@users.noreply.github.com>
2023-06-27 16:27:30 -0400

    Issue production certificate using acme.sh (#8)

--- examples/acme.sh/register-and-issue.sh
+++ examples/acme.sh/register-and-issue.sh
@@ -10,10 +10,21 @@ if [ ! -s creds.json ]; then
   curl -X POST ${LOCALCERT_API}/register > creds.json
 fi
 
-# Issue a certificate with LE (staging env)
+# Issue a certificate
 export ACMEDNS_SUBDOMAIN=$(jq -r .subdomain creds.json)
 export ACMEDNS_FULLDOMAIN=$(jq -r .fulldomain creds.json)
 export ACMEDNS_USERNAME=$(jq -r .username creds.json)
 export ACMEDNS_PASSWORD=$(jq -r .password creds.json)
-~/.acme.sh/acme.sh --issue --dns dns_acmedns -d ${ACMEDNS_FULLDOMAIN} --staging
+
+# First staging environment
+~/.acme.sh/acme.sh --issue --dnssleep 1 --dns dns_acmedns --force -d ${ACMEDNS_FULLDOMAIN} --staging
+
+# Cleanup
+~/.acme.sh/acme.sh --remove -d ${ACMEDNS_FULLDOMAIN} --staging
+
+if [[ $1 == "prod" ]]; then
+  # Try prod if staging worked (force Let's Encrypt)
+  # Only do this on a cron schedule, so we don't consume too much of the rate limit
+  ~/.acme.sh/acme.sh --issue --dnssleep 1 --dns dns_acmedns --force -d ${ACMEDNS_FULLDOMAIN} --server letsencrypt
+fi
 
