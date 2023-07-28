faaba60ecb9e930079103ee14cf6c713b33db5a7
Robert Alexander <3751418+ralexander-phi@users.noreply.github.com>
2023-07-27 23:39:05 -0400

    Better debugging

--- examples/cert-manager/install.sh
+++ examples/cert-manager/install.sh
@@ -11,25 +11,15 @@ kubectl create secret generic acme-dns --from-file acmedns.json
 
 # install cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml
-sleep 30
+sleep 45
 
 # tell cert-manager to issue and manage a certificate
 kubectl apply -f test-resources.yaml
-sleep 10
-
-# view the certificate request
-kubectl describe certificaterequest
-
-# check the issuer status
-kubectl describe issuer le-staging-issuer
-
-# check the challenge
-kubectl describe challenge
 
 # it will take some time to issue a certificate
-for i in {0..24}
+for i in {0..60}
 do
-  sleep 10
+  sleep 5
   if kubectl get certificate cert-manager-test -o json | jq .status.conditions[0].message | tee /dev/stderr | grep "Certificate is up to date and has not expired"; then
     echo "Success!"
     exit 0
@@ -37,5 +27,11 @@ do
 done
 
 echo "Certificate wasn't issued correctly"
+
+# Debug
+kubectl describe certificaterequest
+kubectl describe issuer le-staging-issuer
+kubectl describe challenge
+
 exit 1
 
